d558f6280da4c3b87c83d756a2f9f253640a0e99
Diego <diegosantos4957@gmail.com>
2026-04-22 18:41:25 -0300

    feat: add role.guard.ts to implement role-based route guarding

---
+++ src/app/core/guards/role.guard.ts
@@ -0,0 +1,12 @@
+import { inject } from '@angular/core'
+import { CanActivateFn } from '@angular/router'
+import { AuthService } from '../services/auth.service'
+
+export const roleGuard: CanActivateFn = (route) => {
+
+  const auth = inject(AuthService)
+
+  const roles = route.data['roles']
+
+  return roles.includes(auth.userValue()?.role)
+}
