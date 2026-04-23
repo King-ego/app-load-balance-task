dd5303f969f8ddce38482f82097edf22bb0469f1
Diego <diegosantos4957@gmail.com>
2026-04-23 08:47:40 -0300

    feat: update app.routes.ts to include role-based routing for admin, company, and member paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,51 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { roleGuard } from './core/guards/role.guard'
+import { UserRole } from './core/models/models'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/features/login/pages/login.component')
+        .then(m => m.LoginComponent)
+  },
+
+  {
+    path: '',
+    loadComponent: () =>
+      import('./core/features/home/pages/home.component')
+        .then(m => m.HomeComponent)
+  },
+  {
+    path: 'admin',
+    canActivate: [roleGuard],
+    data: { roles: [UserRole.SYSTEM_ADMIN] },
+    loadChildren: () =>
+      import('./core/features/admin/admin.routes')
+        .then(m => m.ADMIN_ROUTES)
+  },
+
+  {
+    path: 'company',
+    canActivate: [roleGuard],
+    data: { roles: [UserRole.COMPANY_ADMIN] },
+    loadChildren: () =>
+      import('./core/features/company/company.routes')
+        .then(m => m.COMPANY_ROUTES)
+  },
+
+  {
+    path: 'member',
+    canActivate: [roleGuard],
+    data: { roles: [UserRole.MEMBER] },
+    loadChildren: () =>
+      import('./core/features/member/member.routes')
+        .then(m => m.MEMBER_ROUTES)
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+]
