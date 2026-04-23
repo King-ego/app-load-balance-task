d89171bcd8788ae658bba5fde9e7b58426aab356
Diego <diegosantos4957@gmail.com>
2026-04-23 08:47:30 -0300

    feat: add admin.routes.ts to define admin-related routing

---
+++ src/app/core/features/admin/admin.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router'
+import { AdminDashboardComponent } from './pages/dashboard.component'
+
+export const ADMIN_ROUTES: Routes = [
+  {
+    path: '',
+    component: AdminDashboardComponent
+  }
+]
