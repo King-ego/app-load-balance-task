a91c427ce94d881d8d86200792579b60a920f019
Diego <diegosantos4957@gmail.com>
2026-04-23 08:46:53 -0300

    feat: enhance role.guard.ts to redirect unauthorized users to login

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,12 +1,18 @@
 import { inject } from '@angular/core'
-import { CanActivateFn } from '@angular/router'
+import {CanActivateFn, Router} from '@angular/router'
 import { AuthService } from '../services/auth.service'
 
 export const roleGuard: CanActivateFn = (route) => {
 
   const auth = inject(AuthService)
+  const router = inject(Router)
 
-  const roles = route.data['roles']
+  const roles: string[] = route.data['roles'] ?? []
+  const userRole = auth.userValue()?.role
 
-  return roles.includes(auth.userValue()?.role)
+  if (userRole && roles.includes(userRole)) {
+    return true
+  }
+
+  return router.createUrlTree(['/login'])
 }
