ea01cb4bcfe9928ccdaddbdca532b888e0fd4801
Diego <diegosantos4957@gmail.com>
2026-04-23 08:49:08 -0300

    feat: add lazy loading for CompanyDashboardComponent

---
+++ src/app/core/features/company/company.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+
+export const COMPANY_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/dashboard.component')
+        .then(m => m.CompanyDashboardComponent)
+  }
+]
