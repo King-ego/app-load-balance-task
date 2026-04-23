5204fa3c627c0305a79e416de2c687913dc44def
Diego <diegosantos4957@gmail.com>
2026-04-23 08:48:29 -0300

    feat: add auth interceptor for handling authorization tokens in HTTP requests

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,16 @@
+import {HttpInterceptorFn} from "@angular/common/http";
+import {inject} from "@angular/core";
+import {AuthService} from "../services/auth.service";
+
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
+
+  if (auth.token){
+    req = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${auth.token}`
+      }
+    })
+  }
+  return next(req);
+}
