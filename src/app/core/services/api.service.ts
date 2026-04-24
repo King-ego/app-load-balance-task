746a8d4477662b9fb0cd70de87fc28964376946d
Diego <diegosantos4957@gmail.com>
2026-04-24 11:20:43 -0300

    feat: add getUsers method to ApiService for fetching user data

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,12 +1,22 @@
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {User} from "../models/models";
 
 @Injectable({providedIn: 'root'})
 export class ApiService {
+  private base = 'http://localhost:8099';
+  constructor(private http: HttpClient) {}
+
   getMetrics() {
     return {
       totalUsers: 10,
       totalTasks: 40,
       totalPoints: 500
     }
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.base}/users`);
   }
 }
