6e427ab4a118a68706abcdb04dfbf3bd9e68d163
Diego <diegosantos4957@gmail.com>
2026-04-24 16:50:43 -0300

    reset: SSR to SPA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {provideHttpClient, withInterceptors} from "@angular/common/http";
-import {authInterceptor} from "./core/interceptors/auth.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideClientHydration(),
-    provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([authInterceptor]))]
+  providers: [provideRouter(routes)]
 };
