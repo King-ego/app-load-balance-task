723d4c5e7c2298ef840b439f691dd3950c4464c8
Diego <diegosantos4957@gmail.com>
2026-04-25 11:38:54 -0300

    feat: add admin routes for dashboard component

---
+++ src/app/core/features/admin/admin.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+
+export const ADMIN_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/dashboard/admin.dashboard.component')
+        .then(m => m.AdminDashboardComponent)
+  }
+]
