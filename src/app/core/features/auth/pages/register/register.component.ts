50c3a5e43ce0480e3e0177a8afce91e153029680
Diego <diegosantos4957@gmail.com>
2026-04-26 09:12:36 -0300

    feat: implement Google login functionality in RegisterComponent

--- src/app/core/features/auth/pages/register/register.component.ts
+++ src/app/core/features/auth/pages/register/register.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import {MaterialModule} from "../../../../../shared/material.module";
+import {environment} from "../../../../../../../environment";
+
+declare const google: any;
 
 @Component({
   selector: 'app-pages',
@@ -8,6 +11,30 @@ import {MaterialModule} from "../../../../../shared/material.module";
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    const clientId = environment.googleClientId
+    google.accounts.id.initialize({
+      client_id: clientId,
+      callback: (resp: any) => {
+        console.log(resp.credential)
 
+        // enviar pro backend
+        this.loginGoogle(resp.credential)
+      }
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("googleBtn"),
+      {
+        theme: "outline",
+        size: "large",
+        width: 300
+      }
+    );
+  }
+
+  loginGoogle(token: string) {
+    console.log("JWT Google:", token)
+  }
 }
