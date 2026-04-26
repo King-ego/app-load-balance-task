44f57667faf5c7857c38a64e29814c20ce9c3f79
Diego <diegosantos4957@gmail.com>
2026-04-26 09:15:06 -0300

    feat: add member routes for dashboard component

---
+++ src/app/core/features/member/member.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+
+export const MEMBER_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/dashboard.component')
+        .then(m => m.MemberDashboardComponent)
+  }
+]
