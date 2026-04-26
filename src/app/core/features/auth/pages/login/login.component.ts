c2c42ead57fe315258b9c489ff44289dc1fc4fa8
Diego <diegosantos4957@gmail.com>
2026-04-26 09:15:30 -0300

    feat: add login component with Angular Material card

---
+++ src/app/core/features/auth/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatCardModule} from "@angular/material/card";
+
+@Component({
+  standalone: true,
+  imports: [MatCardModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
