45bffc9c81026e9929b136bc91bafaa5e5bdd27d
Diego <diegosantos4957@gmail.com>
2026-04-27 08:09:48 -0300

    feat: add ApiService for handling HTTP requests and mock data

---
+++ src/app/core/services/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core'
+import {of, delay, Observable} from 'rxjs'
+import {HttpClient} from "@angular/common/http";
+import {User} from "../models";
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  private base = 'http://localhost:8099';
+  constructor(private http: HttpClient) {}
+
+  getSystemMetrics() {
+    return of({
+      totalCompanies: 12,
+      totalUsers: 45,
+      totalTasksCompleted: 320,
+      totalPointsDistributed: 5000
+    }).pipe(delay(500))
+  }
+
+  getCompanies() {
+    return of([
+      { id: '1', name: 'Casa Silva', memberCount: 4 },
+      { id: '2', name: 'Casa Souza', memberCount: 3 }
+    ])
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.base}/users`);
+  }
+
+}
