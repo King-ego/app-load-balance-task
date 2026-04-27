6d65613170c1c6e1bd71cfab3f9e3912f9f55366
Diego <diegosantos4957@gmail.com>
2026-04-27 08:09:26 -0300

    feat: add basic structure for login component template

--- src/app/core/features/auth/pages/login/login.component.ts
+++ src/app/core/features/auth/pages/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from "@angular/material/card";
+import {AfterViewInit, Component} from '@angular/core';
+import {MaterialModule} from "../../../../../shared/material.module";
+import {environment} from "../../../../../../../environment";
+
+declare const google: any;
 
 @Component({
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MaterialModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+
+    google.accounts.id.initialize({
+      client_id: environment.googleClientId,
+      callback: (resp: any) => {
+        this.loginGoogle(resp.credential);
+      }
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("googleBtn"),
+      {
+        theme: "outline",
+        size: "large",
+        width: 280
+      }
+    );
+  }
+
+  loginGoogle(token: string) {
+    console.log("Google token:", token);
+  }
 
 }
