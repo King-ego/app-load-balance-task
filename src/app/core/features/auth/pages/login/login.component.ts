52c67336a33f7901ec02729419432258e15a1efb
Diego <diegosantos4957@gmail.com>
2026-04-28 08:32:04 -0300

    feat: add reactive form for login component with validation

--- src/app/core/features/auth/pages/login/login.component.ts
+++ src/app/core/features/auth/pages/login/login.component.ts
@@ -2,16 +2,29 @@ import {AfterViewInit, Component} from '@angular/core';
 import {MaterialModule} from "../../../../../shared/material.module";
 import {environment} from "../../../../../../../environment";
 import {HeaderComponent} from "../../../../../shared/components/header/header.components";
+import {FormBuilder, Validators, ReactiveFormsModule} from "@angular/forms";
 
 declare const google: any;
 
 @Component({
   standalone: true,
-  imports: [MaterialModule, HeaderComponent],
+  imports: [MaterialModule, HeaderComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements AfterViewInit {
+  constructor(private fb: FormBuilder) {
+  }
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+   });
+
+  submit() {
+    console.log(this.form.value)
+  }
+
   ngAfterViewInit(): void {
 
     google.accounts.id.initialize({
