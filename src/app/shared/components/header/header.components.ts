e815153a228cf60a7c7893cc2654b53b44eef89c
Diego <diegosantos4957@gmail.com>
2026-04-28 08:32:37 -0300

    feat: enhance HeaderComponent with router integration and navigation state checks

--- src/app/shared/components/header/header.components.ts
+++ src/app/shared/components/header/header.components.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {MaterialModule} from "../../material.module";
 import {CommonModule} from "@angular/common";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -10,4 +11,12 @@ import {CommonModule} from "@angular/common";
   imports: [MaterialModule, CommonModule]
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public router: Router) {}
+
+  get isLogin() { return this.router.url === '/auth/login'; }
+  get isRegister() { return this.router.url === '/auth/register'; }
+  get isHome() { return this.router.url === '/home'; }
+
+  get isLoggedIn() { return false; }
+}
