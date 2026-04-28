e109ac01e5e36da659488c7065dff9274d536f7e
Diego <diegosantos4957@gmail.com>
2026-04-28 08:31:34 -0300

    fix: enhance role guard to handle user role checks and error handling

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,19 +1,28 @@
 import { inject } from '@angular/core'
-import {CanActivateFn, Router} from '@angular/router'
+import { CanActivateFn, Router } from '@angular/router'
 import { AuthService } from '../services/auth.service'
-
+import { catchError, map, of } from 'rxjs'
 
 export const roleGuard: CanActivateFn = (route) => {
-
   const auth = inject(AuthService)
   const router = inject(Router)
 
   const roles: string[] = route.data['roles'] ?? []
-  const userRole = auth.userValue()?.role
+  const user = auth.userValue()
 
-  if (userRole && roles.includes(userRole)) {
+  const hasAccess = (role?: string) => !!role && roles.includes(role)
+
+  if (hasAccess(user?.role)) {
     return true
   }
 
-  return router.createUrlTree(['/auth/login'])
+  return auth.me().pipe(
+    map((meUser) => {
+      if (hasAccess(meUser.role)) {
+        return true
+      }
+      return router.createUrlTree(['/auth/login'])
+    }),
+    catchError(() => of(router.createUrlTree(['/auth/login'])))
+  )
 }
