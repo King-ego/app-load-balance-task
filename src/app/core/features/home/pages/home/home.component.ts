33678d2114f345e5483e080ae01cdca4accf8fec
Diego <diegosantos4957@gmail.com>
2026-04-29 09:55:10 -0300

    feat: add HeaderComponent to home component imports

--- src/app/core/features/home/pages/home/home.component.ts
+++ src/app/core/features/home/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import {User} from "../../../../models";
 import {ApiService} from "../../../../services/api.service";
 import {CommonModule} from "@angular/common";
 import {MaterialModule} from "../../../../../shared/material.module";
+import {HeaderComponent} from "../../../../../shared/components/header/header.components";
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
