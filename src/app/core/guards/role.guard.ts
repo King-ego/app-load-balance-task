cfefde97d4b9f8e447d11a7c17695350b8a0aee9
Diego <diegosantos4957@gmail.com>
2026-04-30 08:56:31 -0300

    feat: update role guard to use observable user stream for role access validation

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,24 +1,25 @@
 import { inject } from '@angular/core'
 import { CanActivateFn, Router } from '@angular/router'
 import { AuthService } from '../services/auth.service'
-import { catchError, map, of } from 'rxjs'
+import {catchError, map, Observable, of} from 'rxjs'
+import {User} from "../models";
 
 export const roleGuard: CanActivateFn = (route) => {
   const auth = inject(AuthService)
   const router = inject(Router)
 
   const roles: string[] = route.data['roles'] ?? []
-  const user = auth.userValue()
-
   const hasAccess = (role?: string) => !!role && roles.includes(role)
 
-  if (hasAccess(user?.role)) {
+  if (hasAccess(auth.user?.role)) {
     return true
   }
 
-  return auth.me().pipe(
-    map((meUser) => {
-      if (hasAccess(meUser.role)) {
+  const user$: Observable<User | null> = auth.user$
+
+  return user$.pipe(
+    map((meUser: User | null) => {
+      if (meUser && hasAccess(meUser.role)) {
         return true
       }
       return router.createUrlTree(['/auth/login'])
