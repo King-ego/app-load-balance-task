19a7a98ece4e3724f592344083f30df3a4cf6386
Diego <diegosantos4957@gmail.com>
2026-04-30 07:53:36 -0300

    feat: update AuthService to use base URL for API requests and add user storage management

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
-import { BehaviorSubject, Observable, tap } from 'rxjs'
+import {BehaviorSubject, Observable, switchMap, tap} from 'rxjs'
 import { User, UserRole } from '../models'
 
 type LoginRequest = {
@@ -14,6 +14,7 @@ type LoginResponse = {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private base = 'http://localhost:8099';
   private userSubject = new BehaviorSubject<User | null>(null)
   user$ = this.userSubject.asObservable()
 
@@ -21,7 +22,7 @@ export class AuthService {
 
   login(payload: LoginRequest): Observable<LoginResponse> {
     return this.http
-      .post<LoginResponse>('/auth/login', payload, { withCredentials: true })
+      .post<LoginResponse>(`${this.base}/auth/login`, payload, { withCredentials: true })
       .pipe(
         tap((res) => {
           this.userSubject.next(res.user)
@@ -31,7 +32,7 @@ export class AuthService {
 
   logout(): Observable<void> {
     return this.http
-      .post<void>('/auth/logout', {}, { withCredentials: true })
+      .post<void>(`${this.base}/auth/logout`, {}, { withCredentials: true })
       .pipe(
         tap(() => {
           this.userSubject.next(null)
@@ -39,16 +40,29 @@ export class AuthService {
       )
   }
 
-  me(): Observable<User> {
-    return this.http.get<User>('/auth/me', { withCredentials: true }).pipe(
+/*  me(): Observable<User> {
+    return this.http.get<User>(`${this.base}/auth/me`, { withCredentials: true }).pipe(
       tap((user) => {
         this.userSubject.next(user)
       })
     )
+  }*/
+
+  private saveToStorage(user: User | null) {
+    if (user) {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+    } else {
+      localStorage.removeItem('currentUser');
+    }
   }
 
-  get user() {
+  get user(): User | null {
     return this.userSubject.value
+  }
+
+  setUser(user: User | null) {
+    this.userSubject.next(user);
+    this.saveToStorage(user);
   }
 
   hasRole(roles: UserRole[]) {
